b361a549f90af591a8c2ecd8ef268a9620ae2fc1
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-25 02:07:39 +0300

    made find_sub_graph available for import

--- neochain/nc_graph_handler.py
+++ neochain/nc_graph_handler.py
@@ -44,28 +44,33 @@ def __read_data(input_file=None, weighted=None, delimiter=None):
     # Read data
     print('Reading input data.....', log_type='info')
     try:
-        df_t = pd.read_csv(input_file, delimiter=delimiter, names=headers, comment='#', skipinitialspace=True)
+        graph_df = pd.read_csv(input_file, delimiter=delimiter, names=headers, comment='#', skipinitialspace=True)
     except Exception as e:
         print('Can not read input data! ERROR: {}'.format(e), log_type='error', color='red')
         sys.exit(1)
 
     # Return
-    return df_t
+    return graph_df
 
 
 # Create a function for sub-graph
-def __find_sub_graph(data_frame_t=None, top_n_communities_t=None):
+def find_sub_graph(input_file=None, weighted=None, delimiter=None, top_n_communities_t=None):
     """
     This function creates a subset of links/edges from the main input file depending on the nodes from
     top 'n' communities
-    :param data_frame_t: (pandas data frame) A pandas data frame with data from time 't'
+    :param input_file (string) A file path to input data at time 't'
+    :param delimiter: (string) Column separator in both input files, default [whitespace] [MUST BE SAME FOR BOTH FILE]
+    :param weighted: (boolean) yes/no, if files have weight column or not [NUMBER OF COLUMN MUST BE SAME IN BOTH FILES]
     :param top_n_communities_t: (list) List of communities containing the member nodes
     :return: (pandas data frame) a data frame of edges that contains all the nodes from top 'n' communities that
     are present in input file
     """
+    # Read data set from time 't'
+    graph_df_t = __read_data(input_file=input_file, weighted=weighted, delimiter=delimiter)
+    print('Time (t) graph size: {}'.format(len(graph_df_t.index)), log_type='info')
 
     # Assign short name
-    df_t = data_frame_t
+    df_t = graph_df_t
 
     # Select unique nodes from top 'n' communities
     comm_nodes = list(itertools.chain.from_iterable(top_n_communities_t))
@@ -93,19 +98,21 @@ def generate_merged_graph(input_dataset_t=None, input_dataset_t1=None, delimiter
     :return: (pandas data frame) A data frame with links form sub graph at time 't' and links from graph at time 't+1'
     """
     # Step 2(a)
-    # Read data set from time 't'
-    graph_df_t = __read_data(input_file=input_dataset_t, weighted=weighted, delimiter=delimiter)
-
     # Get the sub graph from data set at time 't'
-    sub_graph_t = __find_sub_graph(data_frame_t=graph_df_t, top_n_communities_t=top_n_communities)
+    sub_graph_t = find_sub_graph(input_file=input_dataset_t, weighted=weighted, delimiter=delimiter,
+                                 top_n_communities_t=top_n_communities)
+    print('Time (t) sub-graph size: {}'.format(len(sub_graph_t.index)), log_type='info')
 
     # Step 2(b)
     # Read data set from time 't+1
     graph_df_t1 = __read_data(input_file=input_dataset_t1, weighted=weighted, delimiter=delimiter)
+    print('Time (t1) graph size: {}'.format(len(graph_df_t1.index)), log_type='info')
 
     # Creating a merged graph [G(t,t1)]
+    print('Creating merged graph.....', log_type='info')
     frames = [sub_graph_t, graph_df_t1]
     merged_graph_tt1 = pd.concat(frames)
+    print('Merged graph creation complete!', log_type='info')
 
     # Return
     return merged_graph_tt1
