875e20e3201ef0f378ff2ecddf502cd5c63c908a
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-24 21:31:43 +0300

    added version and docstring

--- neochain/__init__.py
+++ neochain/__init__.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+This package helps to detect communities in blockchain transaction data
+by using well known community detection algorithms and also can observe
+evolution in blockchain network by observing changes in communities in
+blockchain
+
+To install: python setup.py install
+To use: import neochain as nc
+"""
+
+
 # Source code meta data
 __author__ = 'Dalwar Hossain'
 __email__ = 'dalwar.hossain@protonmail.com'
@@ -9,6 +20,7 @@ __email__ = 'dalwar.hossain@protonmail.com'
 # Handle imports
 from nc_community_handler import find_communities, find_top_n_communities
 from nc_graph_handler import generate_merged_graph
+
 
 # Version
 __version__ = 1.0
