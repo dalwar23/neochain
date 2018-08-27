679c7645abe1d545d84aa4cf0cbddead7463829b
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-27 13:22:20 +0300

    added release information file

---
+++ neochain/_release_info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# Source code meta data
+__author__ = 'Dalwar Hossain'
+__email__ = 'dalwar.hossain@protonmail.com'
+
+
+# Version
+__version__ = '1.0'
+__release__ = '1.0b1'
