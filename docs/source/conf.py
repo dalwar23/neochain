06a53ecfd69a0f8e5a4e2604c5e8faf6a01a3e46
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-28 01:53:16 +0300

    re-loading config with mocking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,9 +97,11 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+# Custom configurations
 html_show_sourcelink = False
 html_show_sphinx = False
 add_module_names = False
+autodoc_mock_imports= ['infomap']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
