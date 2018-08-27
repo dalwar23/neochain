b85ae7200d51674e4abcf169a08d70d40baf354b
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-28 02:11:52 +0300

    re-loading config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,6 +102,7 @@ html_show_sourcelink = False
 html_show_sphinx = False
 add_module_names = False
 autodoc_mock_imports= ['infomap']
+autodoc_warningiserror = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
