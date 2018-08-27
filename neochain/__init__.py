38bbdb92831b465fb7d50e94a908e7a93410f660
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-27 13:27:03 +0300

    removed version variable from __init__.py

--- neochain/__init__.py
+++ neochain/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This package helps to detect communities in blockchain transaction data
+This package detects communities in blockchain transaction data
 by using well known community detection algorithms and also can observe
 evolution in blockchain network by observing changes in communities in
 blockchain
@@ -20,7 +20,3 @@ __email__ = 'dalwar.hossain@protonmail.com'
 # Handle imports
 from nc_community_handler import find_communities, find_top_n_communities
 from nc_graph_handler import find_sub_graph, generate_merged_graph
-
-
-# Version
-__version__ = 1.0
